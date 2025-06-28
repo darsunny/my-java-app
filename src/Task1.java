a2cfdd8384e60b169e429c2ec01ffd74fdc0828c
darsunny <dairydasha@gmail.com>
2025-06-28 17:03:49 +0400

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,55 @@
+public class Task1
+{ //main метод
+    public static void main(String[] args) {
+     //Арифметические операторы
+        /* Задача 1
+
+        int b = 5;
+        int c = 3;
+        int a = (4 * (b + c- 1) / 2);
+        System.out.println(a);
+                                */
+        /* Задача 2
+
+        int n = 26;
+        int a = n/10;
+        int b = n%10;
+            System.out.println(a+b);  */
+
+        /* Задача 3
+
+        int n = 126;
+        int a = n/100;
+        int b = (n/10) % 10;
+        int c = n%10;
+            System.out.println(a+b+c); */
+
+        /* Задача 4
+
+        double nDouble = 12.6;
+        int n = (int) nDouble;
+            System.out.println(n); */
+
+        /* Задача 5
+
+        int q = 21;
+        int w = 8;
+        int a = q / w;
+        int b = q % w;
+            System.out.println(q + "/" + w + " = " + a + " и " + b + " в остатке "); */
+
+        /*  Задача *
+
+        int a = 1;
+        int b = 2;
+
+        int c = a;
+        a = b;
+        b = c;
+            System.out.println("a = " + a);
+            System.out.println("b = " +b); */
+
+
+
+    }
+}
---
+++ src/Task2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Task2 { //main метод
+    public static void main(String[] args) {
+        //Операторы управления
+        //Задача 1
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи число: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        System.out.println(number % 2 == 0 ? "Четное": "Нечетное");*/
+
+        /*Задача 2
+        System.out.println("Введи t: ");
+        int t = scanner.nextInt();
+        if (t > -5) {
+            System.out.println("Warm");
+        } else if (t <= -5 && t > -20) {
+            System.out.println("Normal");
+        } else {
+            System.out.println("Cold");
+        }*/
+
+        /*Задача 3
+        for (int i = 10; i <= 20; i++) {
+            System.out.println(i * i);
+        }*/
+
+        Задача 4
+
+
+
+    }
+
+
+}
+
+
